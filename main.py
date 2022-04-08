cc87d99c067bf95da2598ec8ba205e8b48b4a1ca
Geoffrey Cramer <gcwill70@gmail.com>
2022-04-08 10:43:28 -0400

    create separate workflow files for each repo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from github import Github, UnknownObjectException
 def run_foreach(args):
     # env init
     monorepo_name = 'action_foreach_run'
-    work_dir = os.environ.get('GITHUB_WORKSPACE', '/usr/src/app') + f'/{monorepo_name}'
+    work_dir = os.environ.get('GITHUB_WORKSPACE', '/github/workspace') + f'/{monorepo_name}'
     # GitHub init
     github = Github(args['token'])
     org = github.get_organization(os.environ["GITHUB_REPOSITORY_OWNER"])
@@ -25,30 +25,28 @@ def run_foreach(args):
     monorepo = Repo.clone_from(remote.clone_url, work_dir)
     workflow_path = f'{work_dir}/.github/workflows'
     os.makedirs(workflow_path, exist_ok = True)
-    with open(f'{workflow_path}/workflow.yml', "w") as f:
-        f.write(f"name: Foreach Action\n")
-        f.write(f"on: [push]\n")
-        f.write(f"jobs:\n")
     # Fill in monorepo
     for full_name in args['repos'].split(','):
         owner, name = full_name.split('/')
-        # Clone each repo
-        repo_dir = f"{work_dir}/{name}"
+        repo_prefix = f'{owner}-{name}'
+        # Create workflow file
+        with open(f'{workflow_path}/{repo_prefix}-workflow.yml', "w") as f:
+            f.write(f'env:\n')
+            f.write(f'  REPO_DIR: {repo_prefix}\n')
+            f.write(f'{args["action"]}\n')
+        # Clone repo
+        repo_dir = f"{work_dir}/{repo_prefix}"
         if os.path.exists(repo_dir):
             shutil.rmtree(repo_dir)
         Repo.clone_from(f'https://{args["token"]}:x-oauth-basic@github.com/{owner}/{name}.git', repo_dir)
         shutil.rmtree(f'{repo_dir}/.git')
-        # Copy action job
-        with open(f'{workflow_path}/workflow.yml', "a+") as f:
-            f.write(f'  action-{name}:\n')
-            f.write(f'{args["loop"]}\n')
     # Commit to remote
     monorepo.git.add('.')
     monorepo.index.commit(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
     monorepo.git.push('--set-upstream', monorepo.remote().name, 'master', '--force')
 
 def parse_env():
-    inputs = ['token', 'repos', 'loop']
+    inputs = ['token', 'repos', 'action']
     args = {}
     for input in inputs:
         args[input] = core.get_input(input, trim_whitespace = False)
