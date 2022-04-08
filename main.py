2521578395f02defe66819f92f416603ca37b499
Geoffrey Cramer <gcwill70@gmail.com>
2022-04-08 13:07:50 -0400

    WIP: clean up env variables

--- main.py
+++ main.py
@@ -7,59 +7,55 @@ from git import Repo
 from github import Github, UnknownObjectException
 
 
-def run_foreach(args):
-    # env init
-    monorepo_name = 'action_foreach_run'
-    work_dir = os.environ.get('GITHUB_WORKSPACE', '/github/workspace') + f'/{monorepo_name}'
+def run_foreach():
+    # Get monorepo owner & name
+    mr_owner, mr_name = core.get_input('monorepo').split('/')
     # GitHub init
-    github = Github(args['token'])
-    org = github.get_organization(os.environ["GITHUB_REPOSITORY_OWNER"])
+    gh_api = Github(os.environ['GITHUB_TOKEN'])
+    # Clone monorepo
+    org = gh_api.get_organization(mr_owner)
     try:
-        remote = org.get_repo(monorepo_name)
+        remote = org.get_repo(mr_name)
     except UnknownObjectException as e:
         if e.status == 404:
-            remote = org.create_repo(monorepo_name)
-    # Clone monorepo
-    if os.path.exists(work_dir):
-        shutil.rmtree(work_dir)
-    monorepo = Repo.clone_from(remote.clone_url, work_dir)
-    workflow_path = f'{work_dir}/.github/workflows'
-    os.makedirs(workflow_path, exist_ok = True)
-    # Fill in monorepo
-    for full_name in args['repos'].split(','):
+            remote = org.create_repo(mr_name)
+    workdir = f'{os.environ["GITHUB_WORKSPACE"]}/{mr_name}'
+    if os.path.exists(workdir):
+        shutil.rmtree(workdir)
+    monorepo = Repo.clone_from(remote.clone_url, workdir)
+    # Set up workflows path
+    wf_dir = f'{workdir}/.github/workflows'
+    os.makedirs(wf_dir, exist_ok = True)
+    # Clone each repo into monorepo
+    mr_workdir = f'{workdir}/{core.get_input("monorepo_workdir")}'
+    for full_name in core.get_input('repos').split(','):
         owner, name = full_name.split('/')
         repo_prefix = f'{owner}-{name}'
+        repo_dir = f'{mr_workdir}/{repo_prefix}'
         # Create workflow file
-        with open(f'{workflow_path}/{repo_prefix}-workflow.yml', "w") as f:
+        with open(f'{wf_dir}/{repo_prefix}-workflow.yml', "w") as f:
             f.write(f'env:\n')
-            f.write(f'  REPO_DIR: {repo_prefix}\n')
-            f.write(f'{args["action"]}\n')
+            f.write(f'  REPO: {full_name}\n')
+            f.write(f'  REPO_DIR: {os.path.relpath(mr_workdir, workdir)}\n')
+            f.write(f'{core.get_input("action")}\n')
         # Clone repo
-        repo_dir = f"{work_dir}/{repo_prefix}"
         if os.path.exists(repo_dir):
             shutil.rmtree(repo_dir)
-        Repo.clone_from(f'https://{args["token"]}:x-oauth-basic@github.com/{owner}/{name}.git', repo_dir)
-        shutil.rmtree(f'{repo_dir}/.git')
+        Repo.clone_from(f'https://{os.environ["GITHUB_TOKEN"]}:x-oauth-basic@github.com/{owner}/{name}.git', repo_dir)
+        shutil.rmtree(f'{repo_dir}/.git') # deinit as git repo so it is pushed to GH properly
     # Commit to remote
     monorepo.git.add('.')
     monorepo.index.commit(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
     monorepo.git.push('--set-upstream', monorepo.remote().name, 'master', '--force')
 
-def parse_env():
-    inputs = ['token', 'repos', 'action']
-    args = {}
-    for input in inputs:
-        args[input] = core.get_input(input, trim_whitespace = False)
-    return args
-
 def set_default_env():
-    os.environ.setdefault('INPUT_TOKEN', os.environ.get('GITHUB_TOKEN', ''))
+    #os.environ.setdefault('INPUT_MONOREPO', os.environ['GITHUB_REPOSITORY'])
+    pass
 
 def main():
     try:
         set_default_env()
-        args = parse_env()
-        run_foreach(args)
+        run_foreach()
     except Exception as e:
         core.set_failed(str(e))
         
