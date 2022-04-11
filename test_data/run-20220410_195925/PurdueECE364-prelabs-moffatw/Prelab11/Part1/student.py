f8445d1c0d83a7dcfd468348f6d1cabaacda70a6
Geoffrey Cramer <gcwill70@gmail.com>
2022-04-11 12:34:50 -0400

    delete test_data and old generated workflows

--- test_data/run-20220410_195925/PurdueECE364-prelabs-moffatw/Prelab11/Part1/student.py
+++
@@ -1,9 +0,0 @@
-
-
-class Student:
-
-    def __init__(self, first, last, id):
-        self.firstName = first
-        self.lastName = last
-        self.studentID = id
-        self.emailID = last + "." + first + "@university.com"
