893717aa487cf683d7cb92fa9e46b888e3996d8d
Geoffrey Cramer <gcwill70@gmail.com>
2022-04-11 12:34:24 -0400

    dont clone each repo (can use actions/checkout in each workflow)

--- test_unit/test_dircheck.py
+++ test_unit/test_dircheck.py
@@ -3,17 +3,19 @@ from main import main
 from unittest import mock
 
 @mock.patch.dict(os.environ, {
-  "GITHUB_WORKFLOW": "Foreach Dircheck",
+  "GITHUB_WORKFLOW": "Dircheck Test",
   "GITHUB_REF_NAME": "main",
-  "INPUT_WORKDIR": "test_data",
   "INPUT_REPOS": "PurdueECE364/prelabs-moffatw",#,PurdueECE364/prelabs-bbelli,PurdueECE364/prelabs-SOGIST1",
   "INPUT_JOBS": """dircheck:
   runs-on: ubuntu-latest
   steps:
     - uses: actions/checkout@v3
+      with:
+        repository: $REPO
+        token: ${{ secrets.PAT }}
     - uses: PurdueECE/action-dircheck@main
       with:
-        paths: ${{ env.REPO_DIR }}/Prelab08/src
+        paths: Prelab08/src
 """
     })
 def test_dircheck():
