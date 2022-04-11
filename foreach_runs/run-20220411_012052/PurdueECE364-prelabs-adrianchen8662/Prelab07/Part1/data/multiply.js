101188f0dc9de07226bea50fac266784596a2145
Geoffrey Cramer <gcwill70@gmail.com>
2022-04-10 21:27:16 -0400

    delete foreach_runs folder

--- foreach_runs/run-20220411_012052/PurdueECE364-prelabs-adrianchen8662/Prelab07/Part1/data/multiply.js
+++
@@ -1,7 +0,0 @@
-function multiply ()
-{
-    var a = prompt("Please enter your first integer")
-    var b = prompt("Please enter your second integer")
-    var multiplied = (a*b).toString()
-    alert("The integers multiplied equals: " + multiplied)
-}
